49b3f545c8f6ce59e89b7079f1627dec8d0d5f71
maizaaaa <maizatulhasanah02@gmail.com>
2025-11-25 13:53:55 +0800

    Initial Angular resume site

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
+import { LocationStrategy, HashLocationStrategy}  from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
 })
 export class App {
   protected readonly title = signal('my-resume-angular');
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    NgOptimizedImage
-  ],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
+  template: `
+    <section class="hero-section">
+      <img src="assets/images/a.jpg" alt="My Photo" class="profile-img">
+      <h1>Hi, I'm Maizatul Hasanah</h1>
+      <p>Welcome to my personal resume website!</p>
+    </section>
+    <section class="section-padding">
+      <div class="container">
+        <h2>About Me</h2>
+        <p>I am passionate about coding, design, and learning new technologies.</p>
+      </div>
+    </section>
+  `,
+  styleUrls: ['./home.css']
 })
-export class Home {
-
-}
+export class Home {}
