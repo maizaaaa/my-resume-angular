7611a2355a37e8915fadfc1fa398ad117c49433b
maizaaaa <maizatulhasanah02@gmail.com>
2025-11-25 11:43:50 +0800

    Initial Angular resume site

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
