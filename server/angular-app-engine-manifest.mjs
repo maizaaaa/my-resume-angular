a48f34783370401df7ad699c54449d5a942d984f
maizaaaa <maizatulhasanah02@gmail.com>
2025-11-25 14:02:51 +0800

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://maizaaaa.github.io/my-resume-angular',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
